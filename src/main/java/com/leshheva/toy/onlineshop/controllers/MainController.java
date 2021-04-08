eecb24af8b9f331511ac4cc25cfa949553ea9bb4
julialeshheva <julialeshheva@yandex.ru>
2021-04-08 14:05:25 +0300

    We can press the category and see all products from it

--- src/main/java/com/leshheva/toy/onlineshop/controllers/MainController.java
+++ src/main/java/com/leshheva/toy/onlineshop/controllers/MainController.java
@@ -1,11 +1,55 @@
 package com.leshheva.toy.onlineshop.controllers;
 
+import com.leshheva.toy.onlineshop.entities.Product;
+import com.leshheva.toy.onlineshop.service.CategoryService;
+import com.leshheva.toy.onlineshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
+@RequestMapping("/toyshop")
 public class MainController {
+
+    private CategoryService categoryService;
+    private ProductService productService;
+
+    @Autowired
+    public void setCategoryService(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String showMainPage(Model model){
+        return "index";
+    }
+
+    @GetMapping("/category")
+    public String showCategories(Model model){
+
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "category";
+    }
+
+    @GetMapping("/category/{id}")
+    public  String showProductsInCategory(Model model, @PathVariable(value="id")Long id){
+        model.addAttribute("products", productService.getListOfProductsCurrCategory(id));
+        for (Product product:productService.getListOfProductsCurrCategory(id)
+             ) {
+            System.out.println(product.getTitle() +" "+product.getTitle());
+        }
+        return "product-page";
+    }
+
 
 
 }
--- src/main/java/com/leshheva/toy/onlineshop/service/ProductService.java
+++ src/main/java/com/leshheva/toy/onlineshop/service/ProductService.java
@@ -1,7 +1,23 @@
 package com.leshheva.toy.onlineshop.service;
 
+import com.leshheva.toy.onlineshop.entities.Product;
+import com.leshheva.toy.onlineshop.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getListOfProductsCurrCategory(Long category_id) {
+        return  productRepository.findAllByCategory_Id(category_id);
+    }
 }
